d629f324401b96c0b3a6a517c39500630635c4b3
KohutDM <Kogutdmytro@gmail.com>
2019-04-15 19:07:03 +0300

    Static Factory kohutDM Changes to Usage

--- kohutDM/Usage.php
+++ kohutDM/Usage.php
@@ -3,6 +3,7 @@
 use school5\kohutDM\SimpleFactory;
 use school5\kohutDM\Pool;
 use school5\kohutDM\Singleton;
+use school5\kohutDM\StaticFactory;
 
 define('ROOT',dirname(__FILE__));
 
@@ -11,6 +12,7 @@ require (ROOT . '/FightUnit.php');
 require (ROOT . '/Pool.php');
 require (ROOT . '/CastleGuard.php');
 require (ROOT . '/Singleton.php');
+require (ROOT . '/StaticFactory.php');
 
 /**
  * Simple Factory realization
@@ -19,7 +21,20 @@ echo "----------SIMPLE FACTORY----------<br/><br/>";
 $simpleFactory = new SimpleFactory();
 $myUnit = $simpleFactory->makeFightUnit();
 echo $myUnit->fight() . "<br/><br/>";
+echo "--------------------------------------------------<br/><br/>";
+
+/**
+ * Static Factory realization
+ */
+echo "----------STATIC FACTORY----------<br/><br/>";
+$unit =  StaticFactory::factory('FightUnit');
+echo $unit->fight() . "<br/>";
+$unit =  StaticFactory::factory('CastleGuard');
+echo $unit->kingDefence() . "<br/>";
+$unit =  StaticFactory::factory('Cannon');
+echo "<br/><br/>";
 echo "--------------------------------------------------";
+
 
 /**
  * Pool realization
@@ -73,7 +88,7 @@ var_dump($pool->getFreeCastleGuard());
 echo "<br/><br/>";
 
 echo "There are " . $pool->countCastleGuard() . " guardians in the castle<br/><br/>";
-echo "----------------------------------";
+echo "----------------------------------<br/><br/>";
 
 /**
  * Singleton realization
